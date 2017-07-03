17966e594dcb99bf962490c8805da2446e64324e
zhongk <ohnisama@qq.com>
2017-07-03 11:35:15 +0800

    Update tsdb.py

--- tsdb.py
+++ tsdb.py
@@ -1,4 +1,6 @@
-import socket, select, http.client, json, time
+import socket, select, http.client
+import time, os, sys, queue, json
+
 
 class Connection:
 	def __init__(self, host='localhost', port=4242, timeout=2.0):
@@ -28,7 +30,7 @@ class Connection:
 	
 	def put(self, metric, timestamp, value, **kws):
 		float(value)
-		if not kws:
+		if not tags:
 			raise ValueError('Missing tags')
 		if self.closed:
 			self.connect()
@@ -46,45 +48,57 @@ class Connection:
 			self.close()
 			raise
 
+
 class HTTPConnection:
-	def __init__(self, host='localhost', port=4242, timeout=2.0):
+	def __init__(self, host='localhost', port=4242, timeout=2.0, **kws):
 		self.http = http.client.HTTPConnection(host, port, timeout=timeout)
-		self.config = self.request('GET', '/api/config')
-		self.auto_commit = False
+		self.auto_commit = kws.get('auto_commit', False)
 		self.metrics, self.metrics_length = [], 0
+	
+	@property
+	def config(self):
+		if not hasattr(self, '__config'):
+			self.__config = self.request('GET', '/api/config')
+		return self.__config
 	
 	def close(self):
 		self.http.close()
 	
 	def onRequest(self, request, response):
 		res = None
-		# callback function: self.cb_request(method, url, request, reply|except)
+		# callback function: self.cb_request(method, url, request, reply|exc_info)
 		if hasattr(self, 'cb_request') and callable(self.cb_request):
-			if isinstance(response, http.client.HTTPException):
-				exc = response
-				res = self.cb_request(request['method'], request['url'], request['body'], exc)
+			if isinstance(response, tuple):
+				exc_info = response
+				res = self.cb_request(request['method'], request['url'], request['body'], exc_info)
 			else:
 				res = response['reply']
 				self.cb_request(request['method'], request['url'], request['body'], res)
 		return res
 	
-	def request(self, method, url, body=None):
+	def _request(self, method, url, body=None):
 		self.last_request, self.last_response = dict(method=method, url=url, body=body), None
-		try:
-			self.http.request(method, url, body, headers={"Content-Type":"application/json;charset=UTF-8"})
-			response = self.http.getresponse()
-		except http.client.HTTPException as exc:
-			if self.onRequest(self.last_request, exc) is None:
-				raise exc
-			return None
-		
+		self.http.request(method, url, body, headers={"Content-Type":"application/json;charset=UTF-8"})
+	
+	def get_response(self):
+		response = self.http.getresponse()
 		total_bytes = int(response.getheader('Content-Length'))
 		data = bytearray(total_bytes)
 		response.readinto(data)
 		reply = json.loads(data.decode('utf-8'))
 		self.last_response = dict(status=response.status, reason=response.reason, reply=reply)
-		self.onRequest(self.last_request, self.last_response)
 		return reply
+	
+	def request(self, method, url, body=None):
+		try:
+			self._request(method, url, body)
+			reply = self.get_response()
+			self.onRequest(self.last_request, self.last_response)
+			return reply
+		except:
+			if self.onRequest(self.last_request, sys.exc_info()) is None:
+				raise
+			return None
 	
 	def put(self, metric, timestamp, value, **kws):
 		max_chunk = int(self.config['tsd.http.request.max_chunk'])
@@ -161,25 +175,86 @@ class HTTPConnection:
 			if v.startswith('Name already exists with UID:'): uids['tagv'][k] = v[-6:]
 		return uids
 
-###############################################################################################################################
 
-if __name__ == "__main__":
-	from datetime import datetime
-	from pprint import pprint
-	metric = 'python.tsd.test'
-	start = datetime(2017, 6, 2, 12).timestamp()
-	tsd = HTTPConnection('192.168.17.226')
-#	tsd.put(metric, 0, 10, host='host1')
-#	tsd.put(metric, 0, 11, host='host1', name='test.1')
-#	tsd.put(metric, 0, 12, host='host1', name='test.2')
-#	tsd.put(metric, 0, 20, host='host2')
-#	tsd.put(metric, 0, 21, host='host2', name='test.1')
-#	tsd.put(metric, 0, 22, host='host2', name='test.2')
-#	print(tsd.commit())
-	tsd.query(metric, '2d-ago', tags={'host':'host1'}, aggregator='avg')
-	print(tsd.last_response['reply'])
+class TSDB:
+	from multiprocessing.dummy import Process, Queue, Event
 	
-#	pprint(tsd.annotation('00009A000004000049000005000048', int(time.time())-60, 0, 'Test for annotation 1'))
-#	pprint(tsd.annotation('00009A000005000048000004000049', int(time.time()), 0, 'Test for annotation 2'))
-#	req = {'start':'5m-ago', 'end':'now', 'showTSUIDs':True, 'queries':[{'aggregator':'sum', 'metric':metric, 'tags':{'host':'host1', 'name':'test.1'}}]}
-	#pprint(tsd.request('POST', '/api/query', json.dumps(req)))
+	def __init__(self, host='localhost', port=4242, timeout=2.0, **kws):
+		self.host, self.port, self.timeout = host, port, timeout
+		self.metrics, self.metrics_length = [], 0
+		tsd = HTTPConnection(host, port, timeout)
+		max_chunk = kws.get('max_chunk', 0)
+		if max_chunk <= 0:
+			self.max_chunk = int(tsd.config['tsd.http.request.max_chunk'])
+		else:
+			self.max_chunk = min(max_chunk, int(tsd.config['tsd.http.request.max_chunk']))
+		tsd.close()
+		
+		self._processes = kws.get('processes', os.cpu_count())
+		self._requests = self.Queue(kws.get('qsize', self._processes))
+		self._terminated = self.Event()
+		self._callback = kws.get('callback')
+		self._error_callback = kws.get('error_callback')
+		self._handlers = [self.__new_handler()]
+	
+	def __del__(self):
+		if self._handlers:
+			self.close()
+	
+	def close(self):
+		self.commit()
+		self._terminated.set()
+		for handler in self._handlers:
+			handler.join()
+		self._handlers.clear()
+		self._terminated.clear()
+	
+	def __len__(self):
+		return len(self.metrics)
+	
+	def put(self, metric, timestamp, value, **kws):
+		if not timestamp: timestamp = time.time()
+		tags = {k:v for k,v in kws.items() if v not in (None,'')}
+		datapoint = json.dumps(dict(metric=metric, timestamp=int(timestamp), value=value, tags=tags))
+		if len(datapoint)+self.metrics_length+2 > self.max_chunk:
+			self.commit()
+		
+		self.metrics.append(datapoint)
+		self.metrics_length += len(datapoint) + 2
+		return (self.max_chunk - self.metrics_length)
+	
+	def commit(self):
+		if not self.metrics: return
+		if self._requests.full() and len(self._handlers)<self._processes:
+			self._handlers.append(self.__new_handler())
+		self._requests.put('[' + ', '.join(self.metrics) + ']')
+		self.metrics, self.metrics_length = [], 0
+	
+	def __new_handler(self):
+		handler = self.Process(target=self.__handler, 
+		                       args=(self.host, self.port, self.timeout,
+		                             self._requests, self._terminated,
+		                             self._callback, self._error_callback))
+		handler.start()
+		return handler
+	
+	@staticmethod
+	def __handler(host, port, timeout, requests, terminated, callback, error_callback):
+		tsd = HTTPConnection(host, port, timeout)
+		while True:
+			try:
+				request_body = requests.get(timeout=0.1)
+				reply = tsd.request('POST', '/api/put?details', request_body)
+				if callable(callback):
+					callback(request_body, reply)
+			
+			except queue.Empty:
+				if terminated.is_set():
+					tsd.close()
+					break
+			except:
+				if callable(error_callback):
+					try:
+						error_callback(request_body, sys.exc_info())
+					except:
+						pass
